f766d933cfb8135a3ed46b872c6de7cc2e9027b8
Anca Luca <lucaa@xwiki.com>
2022-11-20 23:26:09 +0100

    BATCHIMP-45: Add CSV file reader based on apache commons-csv * A parent pom was added for additional file iterators * A file iterator based on commons-csv was added, usable with the name   of commonscsv

---
+++ xwiki-batchimport-fileiterators/commonscsv/src/main/java/org/xwiki/batchimport/internal/iterators/CommonsCSVImportFileIterator.java
@@ -0,0 +1,172 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.batchimport.internal.iterators;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.slf4j.Logger;
+import org.xwiki.batchimport.BatchImportConfiguration;
+import org.xwiki.batchimport.ImportFileIterator;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.component.annotation.InstantiationStrategy;
+import org.xwiki.component.descriptor.ComponentInstantiationStrategy;
+import org.xwiki.context.Execution;
+import org.xwiki.context.ExecutionContext;
+import org.xwiki.model.reference.AttachmentReference;
+
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiAttachment;
+import com.xpn.xwiki.doc.XWikiDocument;
+
+/**
+ * @version $Id$
+ */
+@Component("commonscsv")
+@InstantiationStrategy(ComponentInstantiationStrategy.PER_LOOKUP)
+public class CommonsCSVImportFileIterator implements ImportFileIterator
+{
+    @Inject
+    private Execution execution;
+
+    private CSVParser csvparser;
+
+    private Iterator<CSVRecord> csvParserIterator;
+
+    @Inject
+    private Logger logger;
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.batchimport.ImportFileIterator#resetFile(org.xwiki.batchimport.BatchImportConfiguration)
+     */
+    @Override
+    public void resetFile(BatchImportConfiguration config) throws IOException
+    {
+        // if a previous reader is open, close it first and then open the new one
+        this.close();
+
+        // prepare the new reader from the passed config and store it
+        InputStreamReader sourceInputStream = getSourceInputStream(config, getXWikiContext());
+
+        this.csvparser = new CSVParser(sourceInputStream,
+            CSVFormat.RFC4180.withDelimiter(config.getCsvSeparator()).withQuote(config.getCsvTextDelimiter()));
+        this.csvParserIterator = this.csvparser.iterator();
+        logger.debug("Starting to read file " + config.getAttachmentReference() + " as commonscsv");
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.batchimport.ImportFileIterator#readNextLine()
+     */
+    @Override
+    public List<String> readNextLine() throws IOException
+    {
+        if (this.csvParserIterator == null) {
+            return null;
+        }
+        if (this.csvParserIterator.hasNext()) {
+            CSVRecord nextRecord = this.csvParserIterator.next();
+            logger.debug("Reading record " + nextRecord.getRecordNumber() + ": " + nextRecord);
+            List<String> nextLineAsList = new ArrayList<String>();
+            for (String s : nextRecord) {
+                nextLineAsList.add(s);
+            }
+            return nextLineAsList;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param config the import configuration, to read input stream && file from it
+     * @param xcontext the xwiki context
+     * @return the input stream for the file to import
+     * @throws IOException if we fail to read properly the source from the config (e.g. no source configured or the
+     *             attachment configured does not exist).
+     */
+    protected InputStreamReader getSourceInputStream(BatchImportConfiguration config, XWikiContext xcontext)
+        throws IOException
+    {
+        InputStream inputStream = null;
+
+        // if some config input stream is passed, use it, otherwise go look at the attachment reference
+        if (config.getFileInputStream() != null) {
+            inputStream = config.getFileInputStream();
+        } else if (config.getAttachmentReference() != null) {
+            AttachmentReference attachmentRef = config.getAttachmentReference();
+            XWikiAttachment attach = null;
+            try {
+                XWikiDocument attachmentDoc =
+                    xcontext.getWiki().getDocument(attachmentRef.getDocumentReference(), xcontext);
+                attach = attachmentDoc.getAttachment(attachmentRef.getName());
+                if (attach != null) {
+                    inputStream = attach.getContentInputStream(xcontext);
+                } else {
+                    throw new IOException("Could not read csv file, attachment not found " + attachmentRef.getName()
+                        + " for document " + attachmentDoc.getDocumentReference() + " for config " + config.toString());
+                }
+            } catch (XWikiException e) {
+                throw new IOException("Could not read csv file, attachment not found " + attachmentRef.getName()
+                    + " for document " + attachmentRef.getDocumentReference() + " for config " + config.toString());
+            }
+
+        } else {
+            throw new IOException(
+                "Could not read csv file, no source file configured in the batch import config: " + config.toString());
+        }
+
+        return new InputStreamReader(inputStream, config.getEncoding());
+    }
+
+    protected XWikiContext getXWikiContext()
+    {
+        ExecutionContext ec = execution.getContext();
+        XWikiContext xwikicontext = (XWikiContext) ec.getProperty(XWikiContext.EXECUTIONCONTEXT_KEY);
+        return xwikicontext;
+    }
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @see org.xwiki.batchimport.ImportFileIterator#close()
+     */
+    @Override
+    public void close() throws IOException
+    {
+        if (this.csvparser != null) {
+            this.csvparser.close();
+            this.csvparser = null;
+            this.csvParserIterator = null;
+        }
+    }
+}
